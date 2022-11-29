7284f7b453c793f5b64dfe17f723dbcedd15c910
francolemuel <francolemuelolguincharles@gmail.com>
2022-11-29 19:39:05 -0300

    qweqweasdqweqweqweqweasdsqweqwe123456789101112131415

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -255,7 +255,7 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if(letra.length === 1){
-    if(letra === 'a' || 'e' || 'i' || 'o' || 'u'){
+    if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
       return ('Es vocal');
     }else{
       return ('Dato incorrecto');
