e6d5a1156178b6bf7cb099193ee87055f2c38d18
francolemuel <francolemuelolguincharles@gmail.com>
2022-11-29 19:27:35 -0300

    qweqweasdqweqweqweqweasdsqweqwe1234567891011

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -130,7 +130,7 @@ function esImpar(num) {
     return false;
   }else{
     return true;
-  }  
+  }
 }
 
 function elevarAlCuadrado(num) {
@@ -172,7 +172,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  var redondeo = Math.Random();
+  var redondeo = Math.random();
   return redondeo;
 }
 
