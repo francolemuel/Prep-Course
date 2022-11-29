1fcf4e2d333c41a2da1f46d066ff580786f8516c
francolemuel <francolemuelolguincharles@gmail.com>
2022-11-29 16:44:40 -0300

    asd123

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -156,7 +156,7 @@ Math.ceil(num);
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-console.log(Math.random())
+return(Math.random());
 }
 
 function esPositivo(numero) {
@@ -165,21 +165,26 @@ function esPositivo(numero) {
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
 if(numero>=0){
-  console.log('es positivo');
+  return('es positivo');
 }else{
-  console.log('es negativo');
+  return('es negativo');
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var str = 'str';
+  return(str + '!');
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  var nombre = ''
+  var apellido = ''
+  return(nombre + ' ' + apellido);
 
 }
 
@@ -187,6 +192,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+var nombre = ''
 
 }
 
