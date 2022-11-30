fe10f70eb659ffbcf20001a9624ea19115e8cb45
francolemuel <francolemuelolguincharles@gmail.com>
2022-11-30 03:56:21 -0300

    asdasdasdasd1234

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -153,7 +153,7 @@ function operadoresLogicos(num1, num2, num3) {
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
   if(num1 >= 0 && num1 > num2 && num1 > num3){
     return ('Número 1 es mayor y positivo');
-  }else if(num1 <= 0 || num2 <= 0 || num3 <= 0){
+  }else if(num1 < 0 || num2 < 0 || num3 < 0){
     return ('Hay negativos');
   }else if(num3 > num1 && num3 > num2){
     return (num3 + 1);
